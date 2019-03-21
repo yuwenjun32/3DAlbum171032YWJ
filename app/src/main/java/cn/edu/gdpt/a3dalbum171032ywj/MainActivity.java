e3cc7b6cb81bb82c4be89ea2f8bedf77c1b775bc
yuwenjun32 <1398981815@qq.com>
2019-03-21 09:34:14 +0800

    初始化控件

--- app/src/main/java/cn/edu/gdpt/a3dalbum171032ywj/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/a3dalbum171032ywj/MainActivity.java
@@ -2,12 +2,29 @@ package cn.edu.gdpt.a3dalbum171032ywj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextSwitcher;
+
+import com.itheima.coverflow.ui.FeatureCoverFlow;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FeatureCoverFlow coverflow;
+    private TextSwitcher title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        coverflow=(FeatureCoverFlow)findViewById(R.id.fcf_coverfiow);
+        title=(TextSwitcher)findViewById(R.id.ts_title);
+        coverflow.setCoverWidth(315);
+        coverflow.setCoverHeight(488);
+        coverflow.setMaxScaleFactor(1.5f);
+        coverflow.setReflectionGap(0);
+        coverflow.setRotationTreshold(0.5f);
+        coverflow.setScalingThreshold(0.5f);
+        coverflow.setSpacing(0.6f);
     }
 }
